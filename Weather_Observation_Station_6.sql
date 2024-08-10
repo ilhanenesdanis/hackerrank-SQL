2641c3cfa5c7e4a81fed8086196fa35a945903d0
ilhan enes daniş <danisenes3@gmail.com>
2024-08-10 18:19:16 +0300

    Question description

--- Weather_Observation_Station_6.sql
+++ Weather_Observation_Station_6.sql
@@ -1,1 +1,13 @@
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+
+----STATION
+--FIELD - TYPE
+ -ID    - NUMBER
+ -CITY  - VARCHAR2(21)
+ -STATE - VARCHAR2(2)
+ LAT_N  - NUMBER
+ LAT_W  - NUMBER
+*/
 select distinct city from station where left(city,1) in('A','E','I','O','U');
